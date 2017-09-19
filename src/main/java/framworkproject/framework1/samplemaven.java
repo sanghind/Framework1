68dedf7ad1f0275e0c74b7167a607ac4c5f0cd60
sanghind <shindocha@gmail.com>
2017-09-19 00:09:01 -0700

    initial commit

---
+++ src/main/java/framworkproject/framework1/samplemaven.java
@@ -0,0 +1,10 @@
+package framworkproject.framework1;
+
+public class samplemaven {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/test/java/framworkproject/framework1/sample2.java
@@ -0,0 +1,11 @@
+package framworkproject.framework1;
+
+public class sample2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("hello world11");
+
+	}
+}
